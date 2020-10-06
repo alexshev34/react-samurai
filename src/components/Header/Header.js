93e9d80947fa3e59ee7fadac5b4b3946b61292d8
Alex <alexshevtsov2013@gmai.com>
2020-10-06 11:52:49 +0500

    lesson 11

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+         <header className="header">
+            <img src="https://s8.hostingkartinok.com/uploads/images/2020/09/14a4df9e2b0a0481b7df5b7de3ad0fe4.png" alt="" width="50" height="50" />
+        </header>
+    );
+  }
+
+  export default Header;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () =>{
+    return(
+         <nav className="nav">
+             <ul className="item">
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Messages</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Music</a></li>
+            </ul>
+         </nav>
+    );
+  }
+
+  export default Nav;
