108e6583d46428a63f51e607d2a7222889885c95
Alex Shevtsov <alexshevtsov2013@gmail.com>
2020-10-07 12:44:38 +0500

    lesson 24

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,13 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () =>{
+
+  let postData = [
+    {id: 1, message: 'Hi, how are you ?', likesCount: 12},
+    {id: 2, message: "It's my first post", likesCount: 11}
+  
+]
+
     return(
         <div>My Posts
           <div>
@@ -11,8 +18,8 @@ const MyPosts = () =>{
               <button>Remove</button>
           </div>
           <div className={s.posts}>
-            <Post message='Hi, how are you ?' likesCount='0' />
-            <Post message="It's my first post" likesCount='23' />
+            <Post message={postData[0].message} likesCount={postData[0].likesCount} />
+            <Post message={postData[1].message} likesCount={postData[1].likesCount} />
           </div>
         </div>
     );
