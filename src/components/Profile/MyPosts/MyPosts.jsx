f02e6843f45cfa4039e9b23762d4b46156939f85
Alex Shevtsov <alexshevtsov2013@gmail.com>
2020-10-08 11:30:30 +0500

    lesson 32

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,11 +7,18 @@ const MyPosts = (props) =>{
   let postsElements =
       props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} /> )
 
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
+
     return(
         <div>My Posts
           <div>
-              <textarea></textarea>
-              <button>Add Post</button>
+              <textarea ref={newPostElement} ></textarea>
+              <button onClick={ addPost } >Add Post</button>
               <button>Remove</button>
           </div>
           <div className={s.posts}>
